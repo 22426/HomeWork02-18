1f1d7237f9d9663708e555a3f2c2ae7b4deb9ba5
22426 <22426@wc.school.nz>
2025-02-19 15:14:37 +1300

    Starting the MCQ's

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 
 import re
-
-questions = ["What is the capital of NZ?", "What is the best number?", "What is the tallest mountain in NZ?"]
-answers = ["wellington", "eleven", "mountcook"]
+questions = ["What is the capital of NZ?",
+             "What is the best number?",
+             "What is the tallest mountain in NZ?"
+            ]
+possible_answers = {0 : ["1. Wellington", "2. Auckland", "3. Queenstown", "4. Sydney"],
+                    1 : ["1. 11", "2. Eleven", "3. three", "4. None of the above"],
+                    2 : ["1. Mt. Cook", "Mount Cook", "3. Mountain Cook", "4. Aoraki"]
+                    }
+answers = [1, 2, 2]
 score = 0
+var = 0
+
+def error_detection():
+    while
+    try:
+        var = int(input(:))
+    except ValueError:
+        print ("Please enter a number")
+
 
 if __name__ == '__main__':
-    for i in range(0, len(questions)):
+    for i in range(len(questions)):
         print(questions[i])
+        var = i
+        print(possible_answers[i])
         answer = input(":")
         answer = re.sub(r"\s+", "", answer)
         if answer.lower() == answers[i]:
